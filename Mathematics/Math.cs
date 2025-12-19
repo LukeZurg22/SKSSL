1d42f64f9c845744c830847ac7962501d9fd7136
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-12-19 02:40:43 -0600

    Updated Math.cs

--- Mathematics/Math.cs
+++ Mathematics/Math.cs
@@ -1,7 +1,19 @@
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
 namespace SKSSL.Mathematics;
 
 public static class Floats
 {
+    /// <summary>
+    /// Assigned Epsilon Value for <see cref="AreFloatsEqual"/> floating point numerical comparison.
+    /// <value>0.0001f</value>
+    /// </summary>
     private const float Epsilon = 0.0001f;
+    
+    /// <summary>
+    /// Compares two floating point values by the following:<br/>
+    /// Get Result For "Is Math.Abs(a-b) less than <see cref="Epsilon"/>?"
+    /// </summary>
+    /// <returns>Whether the two floating point numbers are equal or not.</returns>
     public static bool AreFloatsEqual(float a, float b) => Math.Abs(a - b) < Epsilon;
 }
