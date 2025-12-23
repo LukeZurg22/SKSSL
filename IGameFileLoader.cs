b9b4e403174527476e7cb9fc1de15e10946266dc
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-12-22 20:55:32 -0600

    Updated IGameFileLoader.cs to become File-Scoped

--- IGameFileLoader.cs
+++ IGameFileLoader.cs
@@ -1,22 +1,21 @@
 // ReSharper disable UnusedMember.Global
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMemberInSuper.Global
-namespace SKSSL
+namespace SKSSL;
+
+/// <summary>
+/// For generic parsers or loaders, typically with YAML files that permit the programme's expansion into loading
+/// serialized game content.
+/// </summary>
+public interface IGameFileLoader
 {
     /// <summary>
-    /// For generic parsers or loaders, typically with YAML files that permit the programme's expansion into loading
-    /// serialized game content.
+    /// Loads game content using the provided folder / file path.
     /// </summary>
-    public interface IGameFileLoader
-    {
-        /// <summary>
-        /// Loads game content using the provided folder / file path.
-        /// </summary>
-        public static abstract void Load(string path);
+    public static abstract void Load(string path);
 
-        /// <summary>
-        /// Clean method used for handling the deletion of any leftover data.
-        /// </summary>
-        public static abstract void HandleClean();
-    }
+    /// <summary>
+    /// Clean method used for handling the deletion of any leftover data.
+    /// </summary>
+    public static abstract void HandleClean();
 }
