a7429759ad119d245e995aa3fe22f7d16bb3753c
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-12-31 01:42:05 -0600

    Organized YAML Types

--- YAML/YamlTypes.cs
+++ YAML/YamlTypes.cs
@@ -3,6 +3,8 @@ using RenderingLibrary.Graphics;
 using YamlDotNet.Serialization;
 using Color = Microsoft.Xna.Framework.Color;
 // ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable ClassNeverInstantiated.Global
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
@@ -27,7 +29,9 @@ public record BaseYamlEntry
     /// <summary>
     /// Searchable, indexable ID.
     /// </summary>
-    public string Id { get; set; }
+    [YamlMember(Alias = "id")]
+    public string ReferenceId { get; set; }
+    
 
 }
 
@@ -80,4 +84,52 @@ public record BaseLocalizedColorableYamlEntry : BaseLocalizedYamlEntry
             _color = value;
         }
     }
+}
+
+public class ComponentYaml
+{
+    [YamlMember(Alias = "type")]
+    public string Type { get; set; }  // e.g., "RenderableComponent"
+
+    // Dictionary for flexible fields (for varied components)
+    public Dictionary<string, object> Fields { get; set; } = new();
+}
+
+/// <summary>
+/// <inheritdoc cref="BaseLocalizedYamlEntry"/>
+/// <code>
+/// In Addition To:
+///   components: (Component Yaml Entries)
+///     - type: (string)
+///       field_1: (varies)
+///       field_2: (varies)
+///       field_3: (varies)
+/// Component fields vary between component type.
+/// </code>
+/// </summary>
+public record EntityYaml : BaseLocalizedYamlEntry
+{
+    /// <summary>
+    /// Optional field for <see cref="EntityYaml"/> which is exclusively for entities.
+    /// </summary>
+    [YamlMember(Alias = "components")]
+    public List<ComponentYaml> Components { get; set; } = [];
+}
+
+public record struct EntityTemplate
+{
+    public EntityTemplate(EntityYaml yaml, Dictionary<Type, object> defaultComponents)
+    {
+        ReferenceId = yaml.ReferenceId;
+        NameKey = yaml.Name;
+        DescriptionKey = yaml.Description;
+        DefaultComponents = defaultComponents;
+    }
+
+    public string ReferenceId { get; init; }
+    public string NameKey { get; init; }
+    public string DescriptionKey { get; init; }
+
+    // Generic default components
+    public IReadOnlyDictionary<Type, object> DefaultComponents { get; init; } = new Dictionary<Type, object>();
 }
