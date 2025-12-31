c8c3ac8db7df5727e50363896ea4c5edfd2daeb2
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-12-31 04:20:52 -0600

    Vastly Improved BaseWorld.cs's Potential.
    Note: BaseWorld should, of course, be overwritten with additional code that a world should have. In the case of KBSL, a voxel world.

--- Scenes/BaseWorld.cs
+++ Scenes/BaseWorld.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.Xna.Framework;
+using SKSSL.ECS;
 using SKSSL.Managers;
 using SKSSL.Registry;
 
@@ -5,7 +8,43 @@ namespace SKSSL.Scenes;
 
 public abstract class BaseWorld
 {
-    public readonly EntityManager _entityManager = new();
-    
+    public readonly EntityManager _entityManager;
+    public List<SKEntity> ActiveEntities => _entityManager.AllEntities.ToList();
     public SKEntity SpawnEntity(string referenceId) => _entityManager.Spawn(referenceId, this);
+
+    private readonly SystemManager _systemManager;
+
+    public BaseWorld()
+    {
+        _entityManager = new EntityManager();
+        _systemManager = new SystemManager();
+        
+    }
+
+    /// <summary>
+    /// Called by <see cref="BaseScene"/> initialization.
+    /// <seealso cref="SceneManager"/>
+    /// </summary>
+    public void Initialize()
+    {
+        _systemManager.RegisterAll(this);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        _systemManager.Update(gameTime);
+    }
+
+    public void Draw(GameTime gameTime)
+    {
+        _systemManager.Draw(gameTime);
+    }
+
+    /// <summary>
+    /// Ensures that this world instance is safely deleted before being replaced.
+    /// </summary>
+    public void Destroy()
+    {
+        _entityManager.WipeAllEntities();
+    }
 }
