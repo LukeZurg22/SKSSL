b66297bed2d224493b9d73f81a36c1b1fb13181f
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-04-15 03:05:09 -0500

    Added Additional utility to CancellableEvent

--- ECS/EntityEvent.cs
+++ ECS/EntityEvent.cs
@@ -1,8 +1,20 @@
+// ReSharper disable UnusedMember.Global
+
 namespace SKSSL.ECS;
 
 public class EntityEvent;
 
 public class CancellableEvent : EntityEvent
 {
-    public bool Cancelled;
+    public bool Cancelled { get; private set; }
+    public void Cancel() => Cancelled = true;
+
+    public CancellableEvent()
+    {
+    }
+
+    public CancellableEvent(bool cancelled)
+    {
+        Cancelled = cancelled;
+    }
 }
